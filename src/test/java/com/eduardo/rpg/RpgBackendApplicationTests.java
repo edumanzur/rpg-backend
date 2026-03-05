1a3eddf5c390fb157aa6e1bb1463506beb325106
edumanzur <edumanzur21@gmail.com>
2026-03-05 16:44:08 -0300

    Começando projeto

---
+++ src/test/java/com/eduardo/rpg/RpgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
